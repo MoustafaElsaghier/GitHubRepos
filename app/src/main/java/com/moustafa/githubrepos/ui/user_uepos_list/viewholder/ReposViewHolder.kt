70c5948e62a91aaeadaf152a067e74ab43e4cba1
Moustafa Elsaghier <moustafaelsaghier@gmail.com>
2022-11-04 01:23:28 +0200

    create view holder for repos list

---
+++ app/src/main/java/com/moustafa/githubrepos/ui/user_uepos_list/viewholder/ReposViewHolder.kt
@@ -0,0 +1,32 @@
+package com.moustafa.githubrepos.ui.user_uepos_list.viewholder
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.moustafa.githubrepos.R
+import com.moustafa.githubrepos.data.db.entities.RepositoryModel
+import com.moustafa.githubrepos.databinding.RepoItemLayoutBinding
+
+class ReposViewHolder(private val binding: RepoItemLayoutBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    @SuppressLint("SetTextI18n")
+    fun bind(repositoryModel: RepositoryModel) {
+        binding.repoModel = repositoryModel
+        binding.repoNumber.text = "# $bindingAdapterPosition"
+    }
+
+    companion object {
+        fun create(parent: ViewGroup): ReposViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.repo_item_layout, parent, false)
+
+            val binding = RepoItemLayoutBinding.bind(view)
+
+            return ReposViewHolder(
+                binding
+            )
+        }
+    }
+}
