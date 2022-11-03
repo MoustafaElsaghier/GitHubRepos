84376477a312b6109747a7683706c86cab119e70
Moustafa Elsaghier <moustafaelsaghier@gmail.com>
2022-11-04 00:07:34 +0200

    create repos adapter

---
+++ app/src/main/java/com/moustafa/githubrepos/ui/user_uepos_list/adapter/RepositoriesRxAdapter.kt
@@ -0,0 +1,40 @@
+package com.moustafa.githubrepos.ui.user_uepos_list.adapter
+
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import com.moustafa.githubrepos.data.models.RepositoryModel
+import com.moustafa.githubrepos.ui.user_uepos_list.viewholder.ReposViewHolder
+
+class RepositoriesRxAdapter : PagingDataAdapter<RepositoryModel, ReposViewHolder>(
+    COMPARATOR
+) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReposViewHolder {
+        return ReposViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: ReposViewHolder, position: Int) {
+        getItem(position)?.let {
+            holder.bind(it)
+        }
+    }
+
+    companion object {
+        private val COMPARATOR = object : DiffUtil.ItemCallback<RepositoryModel>() {
+            override fun areItemsTheSame(
+                oldItem: RepositoryModel,
+                newItem: RepositoryModel
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: RepositoryModel,
+                newItem: RepositoryModel
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
