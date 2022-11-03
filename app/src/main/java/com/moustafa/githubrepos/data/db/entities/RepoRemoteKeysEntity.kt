4b6c4416ab2f174764f88c33aa677b1736846a7f
Moustafa Elsaghier <moustafaelsaghier@gmail.com>
2022-11-03 22:45:31 +0200

    create repo remote keys entity

---
+++ app/src/main/java/com/moustafa/githubrepos/data/db/entities/RepoRemoteKeysEntity.kt
@@ -0,0 +1,14 @@
+package com.moustafa.githubrepos.data.db.entities
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "repository_remote_keys")
+data class RepoRemoteKeysEntity(
+    @PrimaryKey val keyId: Long,
+    val prevKey: Int?,
+    val nextKey: Int?
+) : Parcelable
