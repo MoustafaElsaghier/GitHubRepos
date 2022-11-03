325b7e51b9d379d055b92916349c51b00662e60c
Moustafa Elsaghier <moustafaelsaghier@gmail.com>
2022-11-04 00:08:20 +0200

    declare repositories instance in RepoModule.kt

--- app/src/main/java/com/moustafa/githubrepos/di/RepoModule.kt
+++ app/src/main/java/com/moustafa/githubrepos/di/RepoModule.kt
@@ -1,8 +1,11 @@
 package com.moustafa.githubrepos.di
 
+import com.moustafa.githubrepos.data.remote.RepositoriesRemoteMediator
+import com.moustafa.githubrepos.data.repo.GetRepositoriesRxRepository
 import org.koin.dsl.module
 
 val repoModule = module {
-
+    single { GetRepositoriesRxRepository(get(), get()) }
+    single { RepositoriesRemoteMediator(get(), get()) }
 }
 
