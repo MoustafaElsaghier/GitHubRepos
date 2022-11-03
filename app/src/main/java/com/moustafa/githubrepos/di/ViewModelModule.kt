1ea44bbe7213e1dd87a82ba8e9d14d7a657b1328
Moustafa Elsaghier <moustafaelsaghier@gmail.com>
2022-11-04 00:07:54 +0200

    handle view model to be injected by koin

--- app/src/main/java/com/moustafa/githubrepos/di/ViewModelModule.kt
+++ app/src/main/java/com/moustafa/githubrepos/di/ViewModelModule.kt
@@ -1,7 +1,12 @@
 package com.moustafa.githubrepos.di
 
+import com.moustafa.githubrepos.ui.user_uepos_list.viewmodel.UserRepositoriesViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
 
+    viewModel {
+        UserRepositoriesViewModel(get())
+    }
 }
