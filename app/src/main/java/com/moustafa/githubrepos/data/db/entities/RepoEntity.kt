2c294c36f3e8aa659745849820e686cafb3dfab5
Moustafa Elsaghier <moustafaelsaghier@gmail.com>
2022-11-03 02:23:15 +0200

    create entity of repository

---
+++ app/src/main/java/com/moustafa/githubrepos/data/db/entities/RepoEntity.kt
@@ -0,0 +1,21 @@
+package com.moustafa.githubrepos.data.db.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "repos_table")
+data class RepoEntity(
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    @ColumnInfo(defaultValue = "")
+    val name: String,
+    @ColumnInfo(defaultValue = "")
+    val full_name: String,
+    val watchers_count: Int,
+    @ColumnInfo(defaultValue = "")
+    val description: String,
+    @ColumnInfo(defaultValue = "")
+    val language: String,
+) : Serializable
