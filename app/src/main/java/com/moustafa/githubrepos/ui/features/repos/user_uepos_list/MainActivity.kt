6f5e51b277d237c921510654c1606232403794c7
Moustafa Elsaghier <moustafaelsaghier@gmail.com>
2022-11-05 21:01:56 +0200

    update activity code to fit navigation from screen to other

--- app/src/main/java/com/moustafa/githubrepos/ui/features/repos/user_uepos_list/MainActivity.kt
+++ app/src/main/java/com/moustafa/githubrepos/ui/features/repos/user_uepos_list/MainActivity.kt
@@ -1,65 +1,34 @@
 package com.moustafa.githubrepos.ui.features.repos.user_uepos_list
 
-import android.app.AlertDialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
-import androidx.paging.LoadState
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.moustafa.githubrepos.R
 import com.moustafa.githubrepos.databinding.ActivityMainBinding
-import com.moustafa.githubrepos.ui.features.repos.user_uepos_list.adapter.LoadingStateAdapter
-import com.moustafa.githubrepos.ui.features.repos.user_uepos_list.adapter.RepositoriesRxAdapter
-import com.moustafa.githubrepos.ui.features.repos.user_uepos_list.viewmodel.UserRepositoriesViewModel
-import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
-    private var adapter = RepositoriesRxAdapter()
-    private val userRepositoriesViewModel: UserRepositoriesViewModel by inject()
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        initReposRecycler()
-        initLiveData()
+        initNavController()
     }
 
-    private fun initReposRecycler() {
-        binding.reposRecycler.adapter = adapter
-        // set bottom footer for showing loader
-        binding.reposRecycler.adapter = adapter.withLoadStateFooter(
-            footer = LoadingStateAdapter()
+    private fun initNavController() {
+        navController = binding.navHostRepositories.getFragment<NavHostFragment>().navController
+
+        NavigationUI.setupWithNavController(
+            binding.toolbarRepos,
+            navController
         )
-        adapter.addLoadStateListener { loadState ->
-            val errorState = loadState.source.append as? LoadState.Error
-                ?: loadState.source.prepend as? LoadState.Error
-                ?: loadState.append as? LoadState.Error
-                ?: loadState.prepend as? LoadState.Error
-
-            errorState?.let {
-                AlertDialog.Builder(this)
-                    .setTitle(R.string.error)
-                    .setMessage(it.error.localizedMessage)
-                    .setNegativeButton(R.string.cancel) { dialog, _ ->
-                        dialog.dismiss()
-                    }
-                    .setPositiveButton(R.string.retry) { _, _ ->
-                        // to retry load data again
-                        adapter.retry()
-                    }
-                    .show()
-            }
-        }
 
     }
 
-    private fun initLiveData() {
-        // hide progressbar once first page loaded
-        binding.progressBar.isVisible = false
-        // observe on changes from the api
-        userRepositoriesViewModel.repositoryModel.observe(this) {
-            adapter.submitData(lifecycle, it)
-        }
-    }
+
 }
