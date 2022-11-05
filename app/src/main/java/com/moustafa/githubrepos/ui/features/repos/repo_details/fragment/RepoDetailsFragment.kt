91aff479d9b654b39612572db8c43a885b11924c
Moustafa Elsaghier <moustafaelsaghier@gmail.com>
2022-11-05 21:58:14 +0200

    add logic for repo details

--- app/src/main/java/com/moustafa/githubrepos/ui/features/repos/repo_details/fragment/RepoDetailsFragment.kt
+++ app/src/main/java/com/moustafa/githubrepos/ui/features/repos/repo_details/fragment/RepoDetailsFragment.kt
@@ -5,16 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.moustafa.githubrepos.R
+import androidx.navigation.fragment.navArgs
+import com.moustafa.githubrepos.databinding.FragmentRepoDetailsBinding
 
 class RepoDetailsFragment : Fragment() {
+    private lateinit var binding: FragmentRepoDetailsBinding
+    private val args: RepoDetailsFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_repo_details, container, false)
+    ): View {
+        binding = FragmentRepoDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.repoModel = args.repoModel
     }
 
 }
