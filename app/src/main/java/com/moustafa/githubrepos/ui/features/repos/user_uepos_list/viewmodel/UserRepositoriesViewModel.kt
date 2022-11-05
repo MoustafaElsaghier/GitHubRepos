0f3bea2d30afba8f12a639c9def1da6f41be60e9
Moustafa Elsaghier <moustafaelsaghier@gmail.com>
2022-11-05 21:59:42 +0200

    add cache in view-model scope

--- app/src/main/java/com/moustafa/githubrepos/ui/features/repos/user_uepos_list/viewmodel/UserRepositoriesViewModel.kt
+++ app/src/main/java/com/moustafa/githubrepos/ui/features/repos/user_uepos_list/viewmodel/UserRepositoriesViewModel.kt
@@ -2,12 +2,15 @@ package com.moustafa.githubrepos.ui.features.repos.user_uepos_list.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
+import androidx.paging.rxjava2.cachedIn
 import com.moustafa.githubrepos.data.db.entities.RepositoryModel
 import com.moustafa.githubrepos.data.repo.GetRepositoriesRxRepository
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 class UserRepositoriesViewModel(private val repository: GetRepositoriesRxRepository) : ViewModel() {
     private val disposable = CompositeDisposable()
@@ -18,10 +21,12 @@ class UserRepositoriesViewModel(private val repository: GetRepositoriesRxReposit
 
     val repositoryModel = MutableLiveData<PagingData<RepositoryModel>>()
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     private fun getRepositories() {
         disposable.add(
             repository
                 .getRepositories()
+                .cachedIn(viewModelScope)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
